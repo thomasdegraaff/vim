7c8912d1d68174bd08aecc3d65f6bc859a432d68
Thomas de Graaff <thomas@thomasdegraaff.nl>
2014-11-05 15:26:13 +0100

    Added php_ctags.sh script to easily create tags for Drupal and Symphony.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 My personal vim config in a repo to easily share it among devices.
 
 Clone this repo to .vim in your homedir, and link the vimrc in this repo to .vimrc in your home-dir.
+
+Make /home/.../.vim/scripts/php_ctags.sh executable and add /home/.../.vim/scripts to the path (.bashrc PATH=$PATH:/home/.../.vim/scripts).
---
+++ scripts/php_ctags.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+if [ "$1" == "" ];then
+echo "Usage: php_ctags [type]";
+echo "Types are : ";
+echo " - drupal";
+echo " - framework";
+exit 0;
+fi;
+if [ "$1" == "drupal" ];then
+echo "running drupal ctags";
+exec ctags --langmap=php:.engine.inc.module.theme.install.php --php-kinds=cdfi --languages=php --recurse --exclude=".svn" --exclude="sites/default/"
+elif [ "$1" == "framework" ];then
+echo "running framework ctags";
+exec ctags -f tags -h ".php" -R \
+--exclude=".svn" \
+--totals=yes \
+#--tag-relative=yes \
+--PHP-kinds=+cf \
+--regex-PHP='/abstract class ([^ ]*)/\1/c/' \
+--regex-PHP='/interface ([^ ]*)/\1/c/' \
+--regex-PHP='/(public |static |abstract |protected |private )+function ([^ (]*)/\2/f/'
+else
+echo "Not a valid type";
+fi;
